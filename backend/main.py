eb35f062eaf719dcf98679e3e1ae02d86b63eea3
Abinaya <abinaya0442@gmail.com>
2026-06-18 23:37:38 +0530

    Fix Vercel CORS

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:5174", "https://truthlens-ai-omega.vercel.app"],
+    allow_origins=[
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://truthlens-ai-omega.vercel.app"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
